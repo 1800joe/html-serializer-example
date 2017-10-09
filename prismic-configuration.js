95690d852ff76c4090fa39610642f2822cf97532
Levi Gable <levimykel@gmail.com>
2017-10-09 15:41:07 +0200

    Update to API endpoint v2

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  apiEndpoint: 'https://your-repo-name.prismic.io/api',
+  apiEndpoint: 'https://your-repo-name.prismic.io/api/v2',
 
   // -- Access token if the Master is not open
   // accessToken: 'xxxxxx',
@@ -12,7 +12,7 @@ module.exports = {
   // -- Links resolution rules
   // This function will be used to generate links to Prismic.io documents
   // As your project grows, you should update this function according to your routes
-  linkResolver: function(doc, ctx) {
+  linkResolver: function(doc) {
     if (doc.type == 'page') return '/page/' + doc.uid;
     return '/';
   },
