3ddafbbd449c60e9d8f2fb618f636cd1db7bfd33
Levi Gable <levimykel@gmail.com>
2017-10-11 14:38:29 +0200

    Update the htmlSerializer

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,3 +1,6 @@
+var PrismicDOM = require('prismic-dom');
+var Elements = PrismicDOM.Elements;
+
 module.exports = {
 
   apiEndpoint: 'https://your-repo-name.prismic.io/api/v2',
@@ -19,16 +22,26 @@ module.exports = {
   
   // -- HTML Serializer
   // This function will be used to chnage the way the html is loaded
-  htmlSerializer: function (element, content) {
-    // Don't wrap images in a <p> tag
-    if (element.type == 'image') {
-      return '<img src="' + element.url + '" alt="' + element.alt + '">';
+  htmlSerializer: function (element, content, children) {
+    switch(element.type) {
+
+      // Add a class to paragraph elements
+      case Elements.paragraph: 
+        return '<p class="paragraph-class">' + children.join('') + '</p>';
+
+      // Don't wrap images in a <p> tag
+      case Elements.image: 
+        return '<img src="' + element.url + '" alt="' + element.alt + '">';
+
+      // Add a class to hyperlinks
+      case Elements.hyperlink:
+        var target = element.data.target ? 'target="' + element.data.target + '" rel="noopener"' : '';
+        var linkUrl = PrismicDOM.Link.url(element.data, module.exports.linkResolver);
+        return '<a class="some-link"' + target + ' href="' + linkUrl + '">' + content + '</a>';
+
+      // Return null to stick with the default behavior
+      default: 
+        return null;
     }
-    // Add a class to hyperlinks & have them open in a new tab
-    if (element.type == 'hyperlink') {
-      return '<a class="some-link" href="' + element.url + '" target="_blank">' + content + '</a>';
-    }
-    // Return null to stick with the default behavior
-    return null;
   }
 };
